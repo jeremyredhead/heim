818d0d60fca804e71fdd0661d630ab56b879bff2
CylonicRaider <CylonicRaider@users.noreply.github.com>
2026-08-16 23:47:55 +0200

    Convert account dialog to native dialog element
    This should avoid keyboard focus escaping the dialog and roaming random
    parts of the page when tabbing around.

--- client/lib/ui/Dialog.js
+++ client/lib/ui/Dialog.js
@@ -22,13 +22,16 @@ export default createReactClass({
     if (ev.target === this.refs.shade) {
       this.props.onClose()
     }
+    // without this, some clicks escape and are not recognized as internal by the Popup (maybe because their target
+    // elements get re-rendered?)
+    ev.stopPropagation()
   },
 
   render() {
     /* eslint-disable jsx-a11y/click-events-have-key-events */
     return (
       <div className="dim-shade dialog-cover fill" ref="shade" onClick={this.onShadeClick}>
-        <Popup className={classNames('dialog', this.props.className)}>
+        <Popup kind="native-modal" className={classNames('dialog', this.props.className)} onDismiss={this.props.onClose}>
           <div className="top-line">
             <div className="logo">
               <div className="emoji emoji-euphoria" />
